10c7f3e2c971cd782973c4dbcd86ec297028e986
emopudge <emiliakudelenskaa@gmail.com>
2026-04-10 17:44:58 +0300

    [fix] fixed prompt

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,45 +1,42 @@
+import os
+from dotenv import load_dotenv
 from gigachat import GigaChat
 import json
 import pandas as pd
+from pathlib import Path
 
-with open('secrets.txt', 'r') as f:
-    key = f.readline().strip()
+load_dotenv()  # автоматически находит и читает .env в корне проекта
+key = os.getenv("GIGACHAT_CREDENTIALS")
+scope = os.getenv("GIGACHAT_SCOPE")
 
-readme = open(r"data/readme.txt").read()
+readme = Path("data/readme.txt").read_text(encoding="utf-8")
 
 def get_features(readme):
     
-    giga = GigaChat(credentials=key, verify_ssl_certs=False)
+    giga = GigaChat(credentials=key, scope=scope, verify_ssl_certs=False)
 
     PROMPT = """Ты — эксперт по анализу табличных данных.
-
 Тебе дано описание датасета:
-
-{readme.txt}
-
+{readme}
 Задача — бинарная классификация.
-Однозначно таргет
 Ответь ТОЛЬКО в JSON формате:
-
 [
-  {
+  {{
     "column": "название столбца или группы",
     "reason": "почему может влиять на таргет",
     "feature_ideas": [
       "какие признаки можно построить"
     ],
     "priority": "от 1 до 10"
-  }
+  }}
 ]
-
 Правила:
 - НЕ придумывай столбцы, которых нет в описании
 - учитывай возможные утечки таргета
 - делай упор на причинно-логическую связь
 - избегай общих фраз
 - НЕ пиши ничего кроме json
-            
-            """
+          """
     request_giga = PROMPT
     response = giga.chat(request_giga)
     response = response.choices[0].message.content
